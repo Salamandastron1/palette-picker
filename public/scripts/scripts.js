dc9a4640e19a64a3ce0d11bf0ff12869d5bc3423
Tim Garrity <tag.garrity@gmail.com>
2018-11-27 19:52:48 -0700

    merge script files

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,11 +1,32 @@
 const colors = document.querySelectorAll('.color')
-const changeColor = e => {
-  console.log('I did it')
-}
-const clickEvent = colors.forEach(color => {
+const changeColor = colors.forEach(color => {
   color.addEventListener('click', changeColor)
 })
 
+function changeColor () {
+  const newColors = findNewColor()
+  console.log(newColors)
+}
 
+function hexGenerator () {
+  const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  let newHex = ['#']
 
+  for(let i = 0; i < 5; i++) {
+    const index = Math.floor(Math.random() * Math.floor(hexValues.length))
+
+    newHex.push(hexValues[index])
+  }
+  return newHex.join('')
+}
+
+function findNewColor() {
+  const activeColors = document.querySelectorAll('active')
+  let newColors = [];
+
+  for(let i = 1; i < activeColors.length; i++) {
+    newColors.push(hexGenerator())
+  }
+  return newColors
+}
 
