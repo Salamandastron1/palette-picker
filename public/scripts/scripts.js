b42c96b279c0478f1cfa85eb42a1a026f2221a33
Tim Garrity <tag.garrity@gmail.com>
2018-11-28 19:39:36 -0700

    add randomization visually, update innertext with hex

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,33 +1,49 @@
-const colors = document.querySelectorAll('.color')
-const colorListeners = colors.forEach(color => {
-  color.addEventListener('click', changeColor)
-})
-
-function changeColor () {
-  const newColors = findNewColor()
-  console.log(newColors)
-}
-
-function hexGenerator () {
-  const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-  let newHex = ['#']
-
-  for(let i = 0; i < 6; i++) {
-    const index = Math.floor(Math.random() * Math.floor(hexValues.length))
-
-    newHex.push(hexValues[index])
+class ColorConstructor {
+  constructor() {
+    this.state = {
+      projects: []
+    }
   }
 
-  return newHex.join('')
-}
-
-function findNewColor() {
-  const activeColors = document.querySelectorAll('.active')
-  let newColors = [];
-
-  for(let i = 0; i < activeColors.length; i++) {
-    newColors.push(hexGenerator())
+  setColors = () => {
+    const colors = document.querySelectorAll('.color')
+    const colorListeners = colors.forEach(color => {
+      color.addEventListener('click', this.findNewColor)
+    })
+    this.findNewColor()
   }
-  return newColors
+
+  hexGenerator = () => {
+    const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let newHex = ['#']
+
+    for(let i = 0; i < 6; i++) {
+      const index = Math.floor(Math.random() * Math.floor(hexValues.length))
+
+      newHex.push(hexValues[index])
+    }
+
+    return newHex.join('')
+  }
+
+  findNewColor = () => {
+    const activeColors = document.querySelectorAll('.active')
+    let newColors = [];
+
+    for(let i = 0; i < activeColors.length; i++) {
+      newColors.push(this.hexGenerator())
+    }
+
+    activeColors.forEach((color, i) => {
+      color.setAttribute('style', `background-color:${newColors[i]};`)
+      color.innerText = newColors[i]
+    })
+  }
 }
+
+const colorConstructor = new ColorConstructor()
+
+colorConstructor.setColors();
+
+
 
