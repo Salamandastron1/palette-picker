2ee6db3dad931cc4bc0eed99c22110f1d6285b20
Tim Garrity <tag.garrity@gmail.com>
2018-12-02 17:03:44 -0700

    removed uneccessary path

--- server.js
+++ server.js
@@ -19,14 +19,6 @@ app.get('/api/v1/projects',(request, response) => {
     })
 })
 
-app.get('/api/v1/projects/:id', (request, response) => {
-  const { id } = request.params
-
-  database('projects').where('id', id).select()
-    .then(project => response.json(project))
-    .catch(error => response.status(500).json({error: error.message}))
-});
-
 app.post('/api/v1/projects', (request, response) => {
   const project = request.body;
   console.log(project)
