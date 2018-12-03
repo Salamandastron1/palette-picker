9918a0a48471a4a0408c303db45e58c514b23cc6
Tim Garrity <tag.garrity@gmail.com>
2018-12-02 17:40:41 -0700

    finish commenting on server file

--- server.js
+++ server.js
@@ -91,56 +91,56 @@ app.get('/api/v1/projects/:project_id/palettes', (request, response) => {
 })
 
 app.post('/api/v1/projects/:project_id/palettes',(request, response) => {
-
+//end point for posting palettes to specific projects using the variable project id
   const palette = request.body;
-
+//parses content of request body and saves
   const { project_id } = request.params
-
+//finds id from url string
 
   if(!palette) {
-
+//bad data?
     return response.status(422).json({error: 'No palette object submitted'})
-
+//send error non identifiable sequence as json
   }
   for(let requiredParameter of ['name', 'hex_1', 'hex_2', 'hex_3', 'hex_4', 'hex_5']) {
-
+//iterates over palette object looking for correct keys
     if(!palette[requiredParameter]) {
-
+//if keys are not found
       return response.status(422).json({error: `Expected format: {name: <STRING>, hex_1: <STRING>, hex_2: <STRING>, hex_3: <STRING>, hex_4: <STRING>, hex_5: <STRING>}. Missing the required parameter of ${requiredParameter}`})
-
+//send error specifying structure of body content
     }
   }
 
   database('palettes').insert({...palette, project_id}, 'id')
-
+//data good. check db. insert spread palettle object with url id, and send back a unqiue identifier 
     .then(paletteIds => {
-
+//resolves promise on success
       response.status(201).json({id: paletteIds[0]})
-
+//send status of posted with the new id for future use as json
     })
     .catch(error => {
-
+//resovles promise on error 
       response.status(500).json({ error: error.message })
-
+//send server error as json
     })
 })
 
 app.delete('/api/v1/projects/:project_id/palettes/:id', (request, response) => {
-
+//api endpoint for deletion of palettes
   const { id } = request.params
-
+//saves url string of id after palettes
 
   database('palettes').where('id', id).del()
-
+//checks db to see if info exists. if so deletes
     .then(id => response.json({message: `Palette with id ${id} was deleted`}))
-
+//resolves promise on success send message as json
     .catch(error => response.status(500).json({error: error.message}))
-
+//resolves promise on failure, send error as json
 })
 
 
 app.listen(app.get('port'), () => {
-
+//direct the entire app to listen for requests on the specified port 
   console.log(`app is running on ${app.get('port')}`);
-
+//tells developer which port is running
 });
