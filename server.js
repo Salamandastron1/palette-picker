909a45e8bf6b436d69c36a9488760e3d795f98c0
Tim Garrity <tag.garrity@gmail.com>
2019-01-26 19:20:00 -0700

    bug/db

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.get('/api/v1/projects',(request, response) => {
 
 app.post('/api/v1/projects', (request, response) => {
   const project = request.body;
+  console.log(project)
   if(!project) {
     return response.status(422).json({error: 'No project object provided'});
   };
